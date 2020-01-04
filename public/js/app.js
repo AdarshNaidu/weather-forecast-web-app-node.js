be9e780a753e7218100785a5eb7ddb631658b862
AdarshNaidu <naidu.adarsh2511@gmail.com>
2020-01-04 20:23:01 +0530

    rendering forecast to the page

--- public/js/app.js
+++ public/js/app.js
@@ -2,15 +2,21 @@ console.log('client side javascript file is loaded');
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message1');
+const messageTwo = document.querySelector('#message2');
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const location = search.value;
+    messageOne.textContent = "Loading...";
     fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
-            if(data.error) return console.log(data.error)
-            console.log(data.location);
-            console.log(data.forecast);
+            if(data.error){
+                messageOne.textContent = data.error;
+                return;
+            }
+            messageOne.textContent = `Location: ${data.location}`;
+            messageTwo.textContent = `Forecast: ${data.forecast}`;
         })
     })
 })
